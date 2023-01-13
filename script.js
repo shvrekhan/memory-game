709c6cb4787c9b58b5bce6ff2f2c8aea357903a3
shvrekhan <shvrekhan@gmail.com>
2023-01-13 15:58:07 +0530

    match message added

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const homeButton = document.querySelector("#home-button");
 const gameBoard = document.querySelector("#game-container");
 const resetBtn = document.querySelector("#btn-reset");
 const homeBtn = document.querySelector("#btn-home");
+const matchMessage = document.querySelector("#match-message")
 
 let firstCard, secondCard;
 let hasFlipped = false;
 let clickedCount = 0;
+let totalMatch = 0;
 
 const colors = [
     "red",
@@ -45,37 +47,42 @@ function createCards(colorArray) {
 createCards(colors);
 
 parentContainer.addEventListener("click", function (event) {
-
-    console.log(event.target.id);
+    const tagName = event.target.tagName;
+    console.log(event.target.tagName);
+    console.log(clickedCount);
     if (event.target.id == "btn-reset") {
         location.reload();
     }
 
-    if (clickedCount == 0) {
+    if (clickedCount == 0 && !event.target.classList.contains("blocked") && tagName == "DIV") {
         event.target.classList.add(event.target.id);
         firstCard = event.target;
+        firstCard.classList.add("blocked");
         clickedCount++;
-    } else if (clickedCount == 1) {
+    } else if (clickedCount == 1 && !event.target.classList.contains("blocked") && tagName == "DIV") {
         event.target.classList.add(event.target.id);
         secondCard = event.target;
+        secondCard.classList.add("blocked");
         clickedCount++;
     }
 
     if (clickedCount == 2) {
         if (firstCard.id != secondCard.id) {
+            matchMessage.textContent = "retry";
+            firstCard.classList.remove("blocked");
+            secondCard.classList.remove("blocked");
             setTimeout(function () {
                 firstCard.classList.remove(firstCard.id);
                 secondCard.classList.remove(secondCard.id);
                 clickedCount = 0;
-            }, 2000);
+                setTimeout(function () {
+                    matchMessage.textContent = "";
+                }, 1000)
+            }, 1000);
         } else if (firstCard.id == secondCard.id) {
-            firstCard.removeEventListener("onclick", function () {
-                console.log("event removed")
-            });
-            secondCard.removeEventListener("onclick", function () {
-                console.log("event removed");
-            });
+            matchMessage.textContent = "you got a match";
             clickedCount = 0;
+            totalMatch++;
         }
 
     }
