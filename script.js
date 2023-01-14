2d469f70264b46d96441187baa0673e2cd21cc20
shvrekhan <shvrekhan@gmail.com>
2023-01-14 10:38:19 +0530

    live score added

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const homeButton = document.querySelector("#home-button");
 const gameBoard = document.querySelector("#game-container");
 const resetBtn = document.querySelector("#btn-reset");
 const homeBtn = document.querySelector("#btn-home");
-const matchMessage = document.querySelector("#match-message")
+const matchMessage = document.querySelector("#match-message");
+const winMessage = document.querySelector("#win-message");
 const createGameColor = document.querySelector("#new-game-color");
 const home = document.querySelector("#home");
-const gameButtons = document.querySelector("#btn-container")
+const gameButtons = document.querySelector("#btn-container");
+const newGame = document.querySelector("#new-game");
+const currentScore = document.querySelector("#score");
+const createNewGame = document.querySelector("#create-new-game");
 
 let firstCard, secondCard;
-let hasFlipped = false;
 let clickedCount = 0;
 let totalMatch = 0;
 
@@ -35,68 +38,24 @@ function shuffleArray(array) {
 
     return array;
 }
-
+let counter = 1;
 function createCards(colorArray) {
     shuffleArray(colors);
     for (let color of colorArray) {
         const backCard = document.createElement("div");
         backCard.setAttribute('id', color);
-        backCard.classList.add("card", "back-card");
-
+        backCard.classList.add("card", "back-card", `${counter++}`);
         gameBoard.appendChild(backCard);
     }
 }
 
 createCards(colors);
-
-parentContainer.addEventListener("click", function (event) {
-    const tagName = event.target.tagName;
-    console.log(event.target.tagName);
-    console.log(clickedCount);
-    if (event.target.id == "btn-reset") {
-        location.reload();
-    }
-
-    if (clickedCount == 0 && !event.target.classList.contains("blocked") && tagName == "DIV") {
-        event.target.classList.add(event.target.id);
-        firstCard = event.target;
-        firstCard.classList.add("blocked");
-        clickedCount++;
-    } else if (clickedCount == 1 && !event.target.classList.contains("blocked") && tagName == "DIV") {
-        event.target.classList.add(event.target.id);
-        secondCard = event.target;
-        secondCard.classList.add("blocked");
-        clickedCount++;
-    }
-
-    if (clickedCount == 2) {
-        if (firstCard.id != secondCard.id) {
-            matchMessage.textContent = "retry";
-            setTimeout(function () {
-                firstCard.classList.remove("blocked");
-                secondCard.classList.remove("blocked");
-                firstCard.classList.remove(firstCard.id);
-                secondCard.classList.remove(secondCard.id);
-                clickedCount = 0;
-            }, 1000);
-            setTimeout(function () {
-                matchMessage.textContent = "";
-            }, 1000)
-        } else if (firstCard.id == secondCard.id) {
-            matchMessage.textContent = "you got a match";
-            clickedCount = 0;
-            totalMatch++;
-        }
-
-    }
-
-}, false)
-
-
 createGameColor.addEventListener("click", function (event) {
     home.style["display"] = "none";
     gameBoard.style["display"] = "flex";
     gameButtons.style["display"] = "flex";
+    currentScore.textContent = `Current Score - ${totalMatch}`;
+
 
 })
 
@@ -104,5 +63,96 @@ homeButton.addEventListener("click", function (event) {
     home.style["display"] = "flex";
     gameBoard.style["display"] = "none";
     gameButtons.style["display"] = "none";
+
+})
+
+gameBoard.addEventListener("click", function (event) {
+    const tag = event.target.tagName
+    // console.log(event.target.classList.contains("blocked"));
+    if (event.target.nodeName == "DIV" && clickedCount < 2) {
+
+        if (clickedCount == 0 && !(event.target.classList.contains("blocked"))) {
+
+            event.target.classList.add(event.target.id);
+            firstCard = event.target;
+            firstCard.classList.add("blocked");
+            clickedCount++;
+
+        } else if (clickedCount == 1 && !(event.target.classList.contains("blocked"))) {
+
+            event.target.classList.add(event.target.id);
+            secondCard = event.target;
+            secondCard.classList.add("blocked");
+            clickedCount++;
+        }
+
+        if (clickedCount == 2) {
+            event.stopPropagation();
+            if (firstCard.id != secondCard.id) {
+
+                matchMessage.textContent = "Match failed !!!";
+
+                setTimeout(function () {
+                    clickedCount = 0;
+                    firstCard.classList.remove("blocked");
+                    secondCard.classList.remove("blocked");
+                    firstCard.classList.remove(firstCard.id);
+                    secondCard.classList.remove(secondCard.id);
+                }, 1000);
+                setTimeout(function () {
+                    matchMessage.textContent = "";
+                }, 1000)
+            } else if (firstCard.id == secondCard.id) {
+                clickedCount = 0;
+                totalMatch++;
+                matchMessage.textContent = "you got a match";
+                currentScore.textContent = `Current Score - ${totalMatch}`;
+                setTimeout(function () {
+                    matchMessage.textContent = "";
+                }, 1000);
+            }
+
+        }
+        console.log(totalMatch);
+
+        if (totalMatch == 5) {
+            setTimeout(function () {
+                winMessage.textContent = "Congratulations! You have successfully matched all the cards and won the memory game!";
+            }, 1000);
+        }
+
+    }
+}, false);
+
+newGame.addEventListener("click", function (event) {
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
+    }
+    shuffleArray(colors);
+    createCards(colors);
+    totalMatch = 0;
+    clickedCount = 0;
+    currentScore.textContent = `Current Score - ${totalMatch}`;
+})
+
+homeBtn.addEventListener("click", function (event) {
+    home.style["display"] = "flex";
+    gameBoard.style["display"] = "none";
+    gameButtons.style["display"] = "none";
+})
+
+createNewGame.addEventListener("click", function (event) {
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
+    }
+    shuffleArray(colors);
+    createCards(colors);
+    totalMatch = 0;
+    clickedCount = 0;
+    currentScore.textContent = `Current Score - ${totalMatch}`;
+    home.style["display"] = "none";
+    gameBoard.style["display"] = "flex";
+    gameButtons.style["display"] = "flex";
+
 
 })
