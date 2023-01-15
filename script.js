be02c663a919a10fe6e49e32576703866902e662
shvrekhan <shvrekhan@gmail.com>
2023-01-15 18:25:11 +0530

    refactor javascript

--- script.js
+++ script.js
@@ -1,48 +1,73 @@
-const parentContainer = document.querySelector("#parent");
-const headerBar = document.querySelector("#header");
-const homeButton = document.querySelector("#home-button");
-const gameBoard = document.querySelector("#game-container");
-const resetBtn = document.querySelector("#btn-reset");
-const homeBtn = document.querySelector("#btn-home");
-const matchMessage = document.querySelector("#match-message");
-const winMessage = document.querySelector("#win-message");
-const createGameColor = document.querySelector("#new-game-color");
-const home = document.querySelector("#home");
-const gameButtons = document.querySelector("#btn-container");
-const newGame = document.querySelector("#new-game");
-const currentScore = document.querySelector("#score");
-const createNewGame = document.querySelector("#create-new-game");
-const bestScore = document.querySelector("#best-score");
-const currentCounts = document.querySelector("#current-counts");
+const parentContainer = document.querySelector("#parent-container");
+const homeLogo = document.querySelector("#home-logo");
+
+const infoBar = document.querySelector("#info-bar");
+const message = document.querySelector("#message");
+const currentResult = document.querySelector("#current-result");
 const timer = document.querySelector("#timer");
-const currentStats = document.querySelector("#current-stat")
 
+const instructions = document.querySelector("#instruction");
 
-let firstCard, secondCard;
-let clickedCount = 0;
-let totalCount = 0;
-let totalMatch = 0;
+const gameBoardContainer = document.querySelector("#game-board-container");
+const gameBoard = document.querySelector("#game-board");
 
-let previousResult = localStorage.getItem("Best");
-if (previousResult == null) {
-    localStorage.setItem("Best", "9999");
+const difficulty = document.querySelector("#difficulty");
+const easy = document.querySelector("#easy");
+const medium = document.querySelector("#medium");
+const hard = document.querySelector("#hard");
+
+const gameControls = document.querySelector("#pause-newgame");
+const pause = document.querySelector("#pause-resume");
+const newGame = document.querySelector("#new-game");
+
+const globalResult = document.querySelector("#global-result");
+
+let totalCardsToMatch = 0;
+let previousCard = null;
+
+const imgArray = [];
+for (let index = 1; index <= 36; index++) {
+    imgArray.push(`${index}.gif`);
 }
 
-const colors = [
-    "pink",
-    "blue",
-    "voilet",
-    "orange",
-    "purple",
-    "pink",
-    "blue",
-    "voilet",
-    "orange",
-    "purple"
-];
+homeLogo.addEventListener("click", function () {
+    showHome();
+});
+
+easy.addEventListener("click", function () {
+    const level = 4;
+    hideHome();
+    createGame(level);
+});
+
+medium.addEventListener("click", function () {
+    const level = 6;
+    hideHome();
+    createGame(level);
+});
+
+hard.addEventListener("click", function () {
+    const level = 8;
+    hideHome(level);
+    createGame(level);
+});
+
+pause.addEventListener("click", function () {
+    console.log("pause");
+});
+
+newGame.addEventListener("click", function () {
+    console.log("new game");
+});
+
+gameBoard.addEventListener("click", function (event) {
+
+})
+
+
 
 function shuffleArray(array) {
-    
+
     for (let index = array.length - 1; index > 0; index--) {
         const randomIndex = Math.floor(Math.random() * (index + 1));
         [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
@@ -51,158 +76,87 @@ function shuffleArray(array) {
     return array;
 }
 
-let counter = 1;
 
-function createCards(colorArray) {
-    shuffleArray(colors);
+function createGame(level = 4) {
+    const shuffledImages = shuffleArray(imgArray);
+    const imageNeeded = shuffledImages.slice(0, Math.floor((level * level) / 2));
+    totalCardsToMatch = imageNeeded.length;
+    const shuffledPaired = shuffleArray([...imageNeeded, ...imageNeeded]);
+    console.log(shuffledPaired);
+
+    if (level == 4) {
+        createDivsForColors(shuffledPaired);
+    } else if (level == 6) {
+        createDivsForColors(shuffledPaired);
+        document.querySelectorAll('#game-board .card').forEach(function (cardNode) {
+            cardNode.style['width'] = `12vw`;
+            cardNode.style['height'] = `12vh`;
+        });
+
+    } else {
+        createDivsForColors(shuffledPaired);
+        document.querySelectorAll('#game-board .card').forEach(function (cardNode) {
+            cardNode.style["width"] = "9vw";
+            cardNode.style["height"] = "9vh";
+        })
+
+    }
+
+
+}
+
+function createDivsForColors(colorArray) {
     for (let color of colorArray) {
-        const backCard = document.createElement("div");
-        backCard.setAttribute('id', color);
-        backCard.classList.add("card", "back-card", `${counter++}`);
-        gameBoard.appendChild(backCard);
+        const newDiv = document.createElement("div");
+        newDiv.dataset.gif = `${color}`;
+
+        const frontImage = document.createElement("div");
+        const image = document.createElement("img");
+        image.classList.add("card");
+        image.src = "./gifs/question-mark-animation.gif";
+        frontImage.appendChild(image);
+
+        const backImage = document.createElement("div");
+        backImage.classList.add("card", "none");
+        backImage.style.background = `url(gifs/${color})`;
+
+        newDiv.appendChild(frontImage);
+        newDiv.appendChild(backImage);
+
+        newDiv.addEventListener("click", handleCardClick, { capture: true });
+        gameBoard.append(newDiv);
     }
-    if (Number(previousResult) == 9999 || previousResult == null) {
-        bestScore.textContent = `Best Score - First time play`;
-    } else {
-        bestScore.textContent = `Best Score - ${previousResult}`;
-    }
+}
+
+// TODO: Implement this function!
+function handleCardClick(event) {
+    // you can use event.target to see which element was clicked
+    event.stopPropagation();
+    console.log("you clicked", event.target.parentNode.parentNode);
+
+    let currentCard = event.target.parentNode.parentNode;
 
 }
 
-createCards(colors);
 
-createGameColor.addEventListener("click", function (event) {
-    home.style["display"] = "none";
-    gameBoard.style["display"] = "flex";
-    gameButtons.style["display"] = "flex";
-    currentScore.textContent = `Current Score - ${totalMatch}`;
-    let i = 0;
-});if (previousResult != null) {
-    bestScore.textContent = `Best Score - ${previousResult}`;
+
+function hideHome() {
+    infoBar.style["display"] = "flex";
+    instructions.style["display"] = "none";
+    gameBoardContainer.style["display"] = "flex";
+    difficulty.style["display"] = "none";
+    gameControls.style["display"] = "flex";
+    globalResult.style["display"] = "none";
 
 }
 
-homeButton.addEventListener("click", function (event) {
+function showHome() {
+    infoBar.style["display"] = "none";
+    instructions.style["display"] = "flex";
+    gameBoardContainer.style["display"] = "none";
+    difficulty.style["display"] = "flex";
+    gameControls.style["display"] = "none";
+    globalResult.style["display"] = "flex";
 
-    home.style["display"] = "flex";
-    gameBoard.style["display"] = "none";
-    gameButtons.style["display"] = "none";
-    currentStats.style["display"] = "none";
-    if (Number(previousResult) == 9999 || previousResult == null) {
-        bestScore.textContent = `Best Score - First time play`;
-    } else {
-
-        bestScore.textContent = `Best Score - ${previousResult}`;
-    }
-
-
-});
-
-gameBoard.addEventListener("click", function (event) {
-    const tag = event.target.tagName;
-    if (event.target.nodeName == "DIV" && clickedCount < 2) {
-
-        if (clickedCount == 0 && !(event.target.classList.contains("blocked"))) {
-
-            event.target.classList.add(event.target.id);
-            firstCard = event.target;
-            firstCard.classList.add("blocked");
-            clickedCount++;
-            totalCount++;
-            currentCounts.textContent = `Total guess - ${totalCount}`;
-
-        } else if (clickedCount == 1 && !(event.target.classList.contains("blocked"))) {
-
-            event.target.classList.add(event.target.id);
-            secondCard = event.target;
-            secondCard.classList.add("blocked");
-            clickedCount++;
-            totalCount++;
-            currentCounts.textContent = `Total guess - ${totalCount}`;
-        }
-
-        if (clickedCount == 2) {
-            event.stopPropagation();
-            if (firstCard.id != secondCard.id) {
-                firstCard.style["border"] = " 0.4rem solid red";
-                secondCard.style["border"] = " 0.4rem solid red";
-                matchMessage.textContent = "Match failed !!!";
-
-                setTimeout(function () {
-                    clickedCount = 0;
-                    firstCard.classList.remove("blocked");
-                    secondCard.classList.remove("blocked");
-                    firstCard.classList.remove(firstCard.id);
-                    secondCard.classList.remove(secondCard.id);
-                    firstCard.style.border = null;
-                    secondCard.style.border = null;
-
-                }, 1000);
-                setTimeout(function () {
-                    matchMessage.textContent = "";
-                }, 1000);
-            } else if (firstCard.id == secondCard.id) {
-                clickedCount = 0;
-                totalMatch++;
-                firstCard.style["border"] = " 0.4rem solid green";
-                secondCard.style["border"] = " 0.4rem solid green";
-                matchMessage.textContent = "You got a match!";
-                currentScore.textContent = `Current Score - ${totalMatch}`;
-                setTimeout(function () {
-                    matchMessage.textContent = "";
-                }, 1000);
-            }
-
-        }
-
-        if (totalMatch == 5) {
-            console.log(previousResult);
-            if (Number(previousResult) > totalCount || previousResult == null) {
-                console.log("ok");
-                localStorage.setItem("Best", JSON.stringify(totalCount));
-            }
-
-            setTimeout(function () {
-                winMessage.textContent = "Congratulations! You have successfully matched all the cards and won the memory game!";
-            }, 1 * 1000);
-            setTimeout(function () {
-                winMessage.textContent = "";
-            }, 1.5 * 1000);
-
-        }
-
-    }
-}, false);
-
-newGame.addEventListener("click", function (event) {
-    for (let index = gameBoard.childNodes.length - 1; index >= 0; index--) {
-        gameBoard.removeChild(gameBoard.childNodes[index]);
-    }
-    shuffleArray(colors);
-    createCards(colors);
-    totalMatch = 0;
-    clickedCount = 0;
-    currentScore.textContent = `Current Score - ${totalMatch}`;
-});
-
-homeBtn.addEventListener("click", function (event) {
-    home.style["display"] = "flex";
-    gameBoard.style["display"] = "none";
-    gameButtons.style["display"] = "none";
-});
-
-createNewGame.addEventListener("click", function (event) {
-
-    for (let index = gameBoard.childNodes.length - 1; index >= 0; index--) {
-        gameBoard.removeChild(gameBoard.childNodes[index]);
-    }
-    shuffleArray(colors);
-    createCards(colors);
-    totalMatch = 0;
-    clickedCount = 0;
-    currentScore.textContent = `Current Score - ${totalMatch}`;
-    home.style["display"] = "none";
-    gameBoard.style["display"] = "flex";
-    gameButtons.style["display"] = "flex";
-});
+}
 
